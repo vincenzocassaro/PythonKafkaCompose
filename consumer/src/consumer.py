560c37c1766bf3fa5c5ede6d539da87bdb66ced2
Vincenzo Cassaro <vincenzocassaro1@gmail.com>
2022-05-31 11:41:16 +0200

    dockerized consumer and producer

--- consumer/src/consumer.py
+++ consumer/src/consumer.py
@@ -1,19 +1,19 @@
 from flask import Flask, Response
 from kafka import KafkaConsumer
-
+import time
 
 app = Flask(__name__)
 
 
 @app.route("/healtcheck")
 def healtcheck():
-    return "health: ok"
+    return {"health": "ok", "time": time.time()}
 
 
 # Consumer API
 @app.route("/topic/<topicname>")
 def get_messages(topicname):
-    consumer = KafkaConsumer(topicname, bootstrap_servers="kafka:9092")
+    consumer = KafkaConsumer(topicname, bootstrap_servers="kafka:9093")
 
     def events():
         for msg in consumer:
